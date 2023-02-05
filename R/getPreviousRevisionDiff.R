1033f9088baf6eb254d34e37ac572172e8da54a6
Eric Anderson <ericanderson@andersones-Macbook-Pro-16.local>
2023-02-05 15:01:10 -0500

    allow any revision for diff

--- R/getPreviousRevisionDiff.R
+++ R/getPreviousRevisionDiff.R
@@ -1,12 +1,13 @@
 #' Visual diff of previous revision of a file to the local version.
 #' 
 #' @description 
-#' Compares the local version of a script with a previous revision.
+#' Compares the current version of a script with a previous revision.
 #' The output will appear in the viewer and only rows where there have been
 #' additions, deletions or modifications in the script will be shown.
 #'
 #' @param .file file path from working directory
-#' @param .previous_revision revision to compare to local file
+#' @param .previous_revision revision to compare to current revision
+#' @param .current_revision current revision (defaults to local copy)
 #' 
 #' @examples 
 #' \dontrun{
@@ -14,21 +15,36 @@
 #' }
 #' 
 #' @export
-getPreviousRevisionDiff <- function(.file, .previous_revision){
+getPreviousRevisionDiff <- function(.file, .previous_revision, .current_revision = NULL){
   
   .previous_revision_temp_file <- tempfile(fileext = glue::glue(".{tools::file_ext(.file)}"))
   
   system(glue::glue("svn export -r {.previous_revision} {.file} {.previous_revision_temp_file}"))
   
+  .previous_revision_header <- glue::glue("{basename(.file)}: Revision {.previous_revision}")
+  
+  .current_revision_temp_file <- tempfile(fileext = glue::glue(".{tools::file_ext(.file)}"))
+  
+  if (is.null(.current_revision)) {
+    
+    system(glue::glue("cp {.file} {.current_revision_temp_file}"))
+    
+    .current_revision_header <- glue::glue("{basename(.file)}: Local")
+    
+  } else {
+    
+    system(glue::glue("svn export -r {.current_revision} {.file} {.current_revision_temp_file}"))
+    
+    .current_revision_header <- glue::glue("{basename(.file)}: Revision {.current_revision}")
+    
+  }
+  
   diffobj::diffFile(
     target = .previous_revision_temp_file,
-    current = .file, 
+    current = .current_revision_temp_file, 
     color.mode = "rgb",
-    mode = "sidebyside",
-    tar.banner = glue::glue("{basename(.file)}: Revision {.previous_revision}"),
-    cur.banner = glue::glue("{basename(.file)}: Local")
+    tar.banner = .previous_revision_header,
+    cur.banner = .current_revision_header
   )
-  
-  
   
 }
