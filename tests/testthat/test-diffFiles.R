941ab9ef3e1abd97e26b30394fae7f59e4fa43fa
michaelmcd18 <michaelm@metrumrg.com>
2023-03-28 13:26:17 -0400

    fix tests

--- tests/testthat/test-diffFiles.R
+++ tests/testthat/test-diffFiles.R
@@ -8,6 +8,6 @@ diff_data <- diffFiles(.file_1 = "file2.txt", .file_2 = "file.txt")
 
 test_that("diffFiles outputs diff between two different files [REV-DNF-001]", {
   expect_true(diff_data@target != diff_data@current)
-  expect_equal(diff_data@target, "something")
-  expect_equal(diff_data@current, "something with some additions")
+  expect_equal(diff_data@target, "something with some additions")
+  expect_equal(diff_data@current, "something")
 })
