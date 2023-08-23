17d9b4607ad7966d5a90d4c10ed1a0e27c8037e2
michaelm <michaelm@metrumrg.com>
2023-08-23 16:13:06 -0400

    update flags call

--- R/svnHelpers.R
+++ R/svnHelpers.R
@@ -2,7 +2,7 @@
 svnCommand <- function(.file, .command, .flags = NULL) {
   
   temp_loc <- file.path(tempfile(fileext = ".xml"))
-  flags <- paste0(c(rep("-", length(.flags))), .flags, collapse = " ")
+  flags <- paste0(rep("-", length(.flags)), .flags, collapse = " ")
   
   if(!is.null(.flags)) {
     flags <- paste0(flags, " ")
