587ab84033d8c99608c721f0dfaae3a3e2dfd75a
michaelm <michaelm@metrumrg.com>
2023-09-20 12:52:47 -0400

    Add tests for render QC summary

---
+++ tests/testthat/test-renderQCSummary.R
@@ -0,0 +1,50 @@
+test_dir <- createRepo()
+withr::local_dir(test_dir)
+
+add_file("file.R", "something")
+add_file("file2.R", "something")
+add_file("file3.R", "something")
+add_file("file4.R", "something")
+add_file("file5.R", "something")
+
+logCreate()
+add_commit("first")
+# Assign all scripts to QC log
+logAssign(file = "file.R")
+logAssign(file = "file2.R")
+logAssign(file = "file3.R")
+logAssign(file = "file4.R")
+logAssign(file = "file5.R")
+
+# Accept a few scripts
+logAccept(file = "file.R")
+logAccept(file = "file2.R")
+logAccept(file = "file3.R")
+
+# Make edit to one file
+add_file("file.R", "something-new")
+add_commit("second")
+
+test_that("renderQCSummary works with valid directory", {
+  
+  expect_silent({
+    renderQCSummary(.dir = logRoot(), .output_dir = logRoot())
+  })
+  
+  # Check that the output file was created
+  expect_true(file.exists(file.path(logRoot(), paste0("qc-summary-", Sys.Date(), ".pdf"))))
+  
+})
+
+test_that("renderQCSummary doesn't save file if directory not given", {
+  
+  temp_dir <- tempdir()
+  
+  expect_silent({
+    renderQCSummary(.dir = logRoot())
+  })
+  
+  # Check that the output file was created
+  expect_true(file.exists(file.path(temp_dir, paste0("qc-summary-", Sys.Date(), ".pdf"))))
+  
+})
