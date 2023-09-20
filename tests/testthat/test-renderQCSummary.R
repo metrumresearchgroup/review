ad64c76c4e148ba11b0ff9b9fb79c576c9b8e6d2
michaelm <michaelm@metrumrg.com>
2023-09-20 14:06:30 -0400

    update renderQC test

--- tests/testthat/test-renderQCSummary.R
+++ tests/testthat/test-renderQCSummary.R
@@ -25,24 +25,18 @@ logAccept(file = "file3.R")
 add_file("file.R", "something-new")
 add_commit("second")
 
+renderQCSummary(.dir = logRoot(), .output_dir = logRoot())
+
 test_that("renderQCSummary works with valid directory", {
-  
-  expect_silent({
-    renderQCSummary(.dir = logRoot(), .output_dir = logRoot())
-  })
-  
   # Check that the output file was created
   expect_true(file.exists(file.path(logRoot(), paste0("qc-summary-", Sys.Date(), ".pdf"))))
   
 })
 
+temp_dir <- tempdir()
+renderQCSummary(.dir = logRoot())
+
 test_that("renderQCSummary doesn't save file if directory not given", {
-  
-  temp_dir <- tempdir()
-  
-  expect_silent({
-    renderQCSummary(.dir = logRoot())
-  })
   
   # Check that the output file was created
   expect_true(file.exists(file.path(temp_dir, paste0("qc-summary-", Sys.Date(), ".pdf"))))
