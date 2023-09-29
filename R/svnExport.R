4e9991ae676c150f9e1b46236e1defd00aa05c64
anderson <andersone@metrumrg.com>
2023-09-29 10:20:49 -0400

    update export

--- R/svnExport.R
+++ R/svnExport.R
@@ -8,8 +8,8 @@
 #' @param .revision A character or numeric value specifying the SVN revision number of the file to be exported.
 #' @param .output_dir A character string specifying the directory where the exported file should be saved.
 #'   Defaults to the current working directory.
+#' @param .return_file Boolean. Should the newly created file name be returned as a string?
 #'
-#' @return NULL invisibly.
 #' 
 #' @examples
 #' \dontrun{
@@ -17,14 +17,13 @@
 #' }
 #' 
 #' @export
-svnExport <- function(.file, .revision, .output_dir = getwd()) {
+svnExport <- function(.file, .revision, .output_dir = getwd(), .return_file = FALSE) {
   
   if (!file.exists(.file)) {
     stop(".file not found")
   }
   
-  .file_command <- paste(
-    .file,
+  .file_rev_path <- 
     file.path(
       .output_dir,
       paste0(
@@ -34,9 +33,9 @@ svnExport <- function(.file, .revision, .output_dir = getwd()) {
         ".",
         tools::file_ext(.file)
       )
-    ),
-    collapse = " "
-  )
+    )
+  
+  .file_command <- paste(.file, .file_rev_path, collapse = " ")
   
   export_try <- tryCatch(
     svnCommand(
@@ -50,6 +49,12 @@ svnExport <- function(.file, .revision, .output_dir = getwd()) {
   
   if (inherits(export_try, "error")) {
     stop("svn export failed")
+  }
+  
+  cli::cli_inform(paste0("File exported: ", .file_rev_path))
+  
+  if (.return_file) {
+    return(.file_rev_path)
   }
   
   return(invisible(NULL))
