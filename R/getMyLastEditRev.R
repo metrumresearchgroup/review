b532a17f0288b27f2bf7a2d6e42e5cc6fe78cc17
michaelm <michaelm@metrumrg.com>
2024-01-22 10:15:24 -0500

    Create function to get revision of user's last modifcation

---
+++ R/getMyLastEditRev.R
@@ -0,0 +1,33 @@
+#' Find revision of user's last modification 
+#' 
+#' @description 
+#' Compares the local version of a script with the most recent version the user
+#' committed. This is a helpful tool when reviewing the changes made after QC
+#' comments have been addressed.
+#'
+#' @param .file file path from working directory
+#' @param .host_name Host name where repository is hosted
+#' 
+#' @examples 
+#' \dontrun{
+#' getMyLastEditRev(.file = "script/data-assembly.Rmd")
+#' }
+#' 
+#' @keywords internal
+getMyLastEditRev <- function(.file, .host_name = "mc1.metrumrg.com") {
+  
+  # Search SVN log 
+  logFile <- svnLog(.file)
+  userSVN <- svnUser(.host_name = .host_name)[["svn"]]
+  
+  authorRevs <-
+    logFile %>% 
+    dplyr::filter(author == userSVN) %>% 
+    dplyr::slice(1)
+  
+  if (nrow(authorRevs) == 0) {
+    cli::cli_abort(glue::glue("User has never modified '{.file}'"))
+  }
+  
+  return(as.numeric(authorRevs$rev))
+}
---
+++ tests/testthat/test-getMyLastEditRev.R
@@ -0,0 +1,67 @@
+this_user <- 
+  user_lookup %>% 
+  dplyr::filter(sys == Sys.info()[["user"]]) %>% 
+  dplyr::pull(svn)
+
+testthat::skip_if(
+  length(this_user) == 0,
+  glue::glue("Skipped svnUser tests because user not found in svn lookup in setup.R")
+)
+
+remote_repo_local <- paste0(tempdir(), "/test")
+
+.command <-
+  glue::glue("svn co svn+ssh://{this_user}@mc1-test.metrumrg.com/common/repo/svn-proj-review-tests {remote_repo_local} -q -q")
+
+system(.command)
+
+setwd(remote_repo_local)
+
+user_res <- svnUser(.host_name = "mc1-test.metrumrg.com")
+
+# Create generalized tests that will pass independent of who is running them
+file1 <- "script/data-assembly/da-combine-studies.Rmd"
+
+svnLog1 <- svnLog(file1)
+
+if (user_res$svn %in% svnLog1$author) {
+  last_rev1 <-
+    svnLog1 %>% 
+    dplyr::filter(author == user_res$svn) %>% 
+    dplyr::slice(1)
+  
+  testthat::test_that("getMyLastEditRev finds the correct revision the user last made an edit", {
+    expect_equal(as.numeric(last_rev1$rev), getMyLastEditRev(file1, .host_name = "mc1-test.metrumrg.com"))
+  })
+}
+
+# Create generalized tests that will pass independent of who is running them
+file2 <- "script/data-assembly/da-functions.R"
+
+svnLog2 <- svnLog(file2)
+
+if (user_res$svn %in% svnLog2$author) {
+  last_rev2 <-
+    svnLog2 %>% 
+    dplyr::filter(author == user_res$svn) %>% 
+    dplyr::slice(1)
+  
+  testthat::test_that("getMyLastEditRev finds the correct revision the user last made an edit", {
+    expect_equal(as.numeric(last_rev2$rev), getMyLastEditRev(file2, .host_name = "mc1-test.metrumrg.com"))
+  })
+}
+
+file3 <- "script/data-assembly/da-study-abc.Rmd"
+
+svnLog3 <- svnLog(file3)
+
+if (user_res$svn %in% svnLog2$author) {
+  last_rev3 <-
+    svnLog3 %>% 
+    dplyr::filter(author == user_res$svn) %>% 
+    dplyr::slice(1)
+  
+  testthat::test_that("getMyLastEditRev finds the correct revision the user last made an edit", {
+    expect_equal(as.numeric(last_rev3$rev), getMyLastEditRev(file3, .host_name = "mc1-test.metrumrg.com"))
+  })
+}
