aff11169cabf656de0c8a07fb3519545a90a97a2
michaelm <michaelm@metrumrg.com>
2024-02-16 10:06:31 -0500

    Update function to diffFigure

--- R/diffFigure.R
+++ R/diffFigure.R
@@ -1,21 +1,26 @@
-#' Create a GIF of original vs modified PDF
+#' Create a GIF of original vs modified figure
 #' 
 #' @description 
-#' Compares the local version of a PDF with the most recent version checked into
+#' Compares the local version of a figure with the most recent version checked into
 #' SVN. A GIF will be created to help the user compare the original file to the 
 #' modified one.
 #'
 #' @param .file1 file path from working directory
 #' @param .file2 file path from working directory
 #' @param .fps frames per second, increase for quicker switching between images
+#' @param .page_num1 specify page number of .file1 to do comparison of (if applicable)
+#' @param .page_num2 specify page number of .file2 to do comparison of (if applicable)
 #' 
 #' @examples 
 #' \dontrun{
-#' diffPDF(.file1 = "example-1.pdf", .file2 = "example-2.pdf", .fps = 1)
+#' diffFigure(
+#'   .file1 = "example-1.pdf", 
+#'   .file2 = "example-2.pdf", 
+#'   .fps = 1)
 #' }
 #' 
 #' @export
-diffPDF <- function(.file1, .file2, .fps = 1) {
+diffFigure <- function(.file1, .file2, .fps = 1, .page_num1 = NULL, .page_num2 = NULL) {
   
   if (!file.exists(.file1)) {
     paste0(.file1, " does not exist")
@@ -29,6 +34,15 @@ diffPDF <- function(.file1, .file2, .fps = 1) {
                 current = .file2)
   
   img_list <- lapply(paths, magick::image_read)
+  
+  if (!is.null(.page_num1)) {
+    img_list$previous <- img_list$previous[.page_num1]
+  }
+  
+  if (!is.null(.page_num2)) {
+    img_list$current <- img_list$current[.page_num2]
+  }
+  
   
   if(length(img_list[["previous"]]) != length(img_list[["current"]])){
     stop("must have same num pages")
