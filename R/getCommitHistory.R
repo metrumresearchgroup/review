a65aa868641caaa16182c75640af0d46ee1fa154
michaelm <michaelm@metrumrg.com>
2024-07-30 16:50:41 -0400

    Add getCommitHistory and mock up svnLog updates

---
+++ R/getCommitHistory.R
@@ -0,0 +1,32 @@
+#' Get Full Commit History
+#'
+#' @description
+#' Returns a data.frame containing every commit made to the repo. 
+#' The information recorded includes the author, revision, date and
+#' commit message.
+#'
+#' @export
+getCommitHistory <- function() {
+  
+  svn_all <- tryCatch(
+    svnCommand("log", .flags = "-v"),
+    error = identity
+  )
+  
+  if (inherits(svn_all, "error")) {
+    stop("svn log failed")
+  }
+  
+  dplyr::bind_rows(svn_all) %>% 
+    tidyr::unnest(paths) %>% 
+    tidyr::unnest(paths) %>% 
+    dplyr::filter(grepl("/", paths, fixed = TRUE)) %>% 
+    dplyr::rename(
+      rev = .attrs,
+      file = paths
+    ) %>% 
+    dplyr::mutate(
+      date = as.Date(substr(date, 1, 10)),
+      file = sub('.', '', file))
+  
+}
